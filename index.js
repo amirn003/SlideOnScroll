f1e12744353cdc2c861262e07b3cae2232e9e27e
amirn003 <amir.naar@gmail.com>
2023-11-07 19:50:19 +0100

    display image while scrolling

--- index.js
+++ index.js
@@ -17,9 +17,20 @@ const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
   console.log(window.scrollY);
-  console.log('INNER', window.innerHeight);
   sliderImages.forEach((sliderImage) => {
-    const slideInAt = (window.scrollY + window.innerHeight);
+    //console.log(sliderImage);
+    const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height / 2);
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+
   })
 }
 
